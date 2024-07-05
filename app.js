18d9a8deeeb6d86d4ffffc1a78bd88f1d60d24a0
Abdul Ahad Hassan <malikabdulahadhassan@gmail.com>
2024-07-06 03:04:48 +0500

    chat-bot-1.2

--- app.js
+++ app.js
@@ -2,18 +2,73 @@
 var userList = document.getElementById("user-imp")
 var botList = document.getElementsByClassName("bot-imp")
 var impValue = document.getElementById("imp-value")
+var typing = document.getElementById("typing")
+
+var match = true
+var arr1 = ["hello","hi","yo","Asalamualikum"]
+var arr2 = ["ma theek","thek","theek","Allah ka sukar ha","Alhamdulilah"]
+var arr3 = ["mujhy addmission lena hai","mujhy addmission chahiye","addmission lena hai","addmission chahiye","addmission chaiye"]
+
+
+
+function lastMessage() {
+    var scrol = document.getElementsByClassName("text-mess");
+    scrol.scrollTop = scrol.scrollHeight;
+  }
 
 
 function botAnswer(){
-    if(event.keyCode == 13 || event.type == "click" ){
+    if(impValue.value == " "){
+        impValue.value = null
+
+    }else{
+
+        userList.innerHTML +=  '<li class="text-end"><span class="green-text">' + impValue.value  + '</span></li>'
         
-        userList.innerHTML +=  '<li class="text-start"> <span> ' + impValue.value  + "</span> </li>" 
+        typing.style.display = "block"
     }
+    // for(var i = 0 ; i < arr1.length ; i++){
 
-    // if( userList ==  ["water", "fire"]){
-    //     userList.innerHTML +=  '<li class=" text-end" >' + impValue.value  + " </li>" 
+
+
+        if( arr1.lastIndexOf(impValue.value) !== -1){
+            setTimeout(function(){userList.innerHTML +=  '<li class=" text-start" ><span>' + 'Hello :) '  + '</span></li>'},2500)
+            setTimeout(function(){userList.innerHTML +=  '<li class=" text-start" ><span>' + 'Kya hal hn ap ke  '  + '</span></li>'; typing.style.display = "none" ;lastMessage()},4000)
+            
+            
+        
+        }
+        else if( arr2.lastIndexOf(impValue.value) !== -1){
+            setTimeout(function(){userList.innerHTML +=  '<li class=" text-start" ><span>' + 'Sun ke acha laga  '  + '</span></li>'},2500)
+            setTimeout(function(){userList.innerHTML +=  '<li class=" text-start" ><span>' + 'toh kya help karo ma ap ki  '  + '</span></li>'; typing.style.display = "none" ;lastMessage()},4000)
+           
+        
+        }
+        else if( arr3.lastIndexOf(impValue.value) !== -1){
+            setTimeout(function(){userList.innerHTML +=  '<li class=" text-start" ><span>' + 'Sun ke acha laga  '  + '</span></li>'},2500)
+            setTimeout(function(){userList.innerHTML +=  '<li class=" text-start" ><span>' + 'toh kya help karo ma ap ki  '  + '</span></li>'; typing.style.display  = "none";lastMessage()},4000)
+        
+        }
+        else{ 
+            setTimeout(function(){userList.innerHTML +=  '<li class=" text-start" ><span>' + 'Samaj nahi saka  '  + '</span></li>' ; typing.style.display  = "none";lastMessage()},2500)
+              
+        }
+
     // }
-    console.log(event)
-
-
+        
+        
+        if(match){
+            impValue.value = ""
+            match = false
+        }
+            match = true
 }
+
+
+function enter123(){
+    if(event.keyCode == 13 ){
+        botAnswer()
+
+    }
+}
+
